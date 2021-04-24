3e515e9a62c4ff2d4b380f4a4419eea63c2ad2bf
Silas Michael Knight <silasfelinus@gmail.com>
2021-04-24 23:16:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
         resp.write item.price
       else
         resp.write "Item not found"
-        resp.status = 404
+        resp.status = 400
       end
     else
       resp.write "Route not found"
