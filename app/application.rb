6a6dc5df619bb25dd2343018692a5a9ac71be7b7
Silas Michael Knight <silasfelinus@gmail.com>
2021-04-24 23:12:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match=="/items/"
+    if req.path.match(/items/)
       item_title = req.path.split("/items/").last #turn /songs/Sorry into Sorry
       item = @@items.find{|s| s.title == item_title}
 
